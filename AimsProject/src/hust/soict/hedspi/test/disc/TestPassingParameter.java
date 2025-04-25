b498d9546603c34dbf7ac64096771c98899ac067
IncuksukEi <incuksuk123@gmail.com>
2025-04-25 20:06:58 +0700

    add test Aims Project

--- AimsProject/src/hust/soict/hedspi/test/disc/TestPassingParameter.java
+++ AimsProject/src/hust/soict/hedspi/test/disc/TestPassingParameter.java
@@ -1,31 +1,40 @@
-import hust.soict.hedspi.aims.disc.DigitalVideoDisc;
+package hust.soict.hedspi.test.disc;
+
+import hust.soict.hedspi.aims.media.DigitalVideoDisc;
 
 public class TestPassingParameter {
     public static void main(String[] args) {
         DigitalVideoDisc jungleDVD = new DigitalVideoDisc("Jungle");
         DigitalVideoDisc cinderellaDVD = new DigitalVideoDisc("Cinderella");
 
-        // Test swap()
+        // Test swap
+        System.out.println("-- Before swap --");
+        System.out.println("jungleDVD title: " + jungleDVD.getTitle());
+        System.out.println("cinderellaDVD title: " + cinderellaDVD.getTitle());
+
         swap(jungleDVD, cinderellaDVD);
-        System.out.println("jungle dvd title: " + jungleDVD.getTitle());
-        System.out.println("cinderella dvd title: " + cinderellaDVD.getTitle());
 
-        // Test changeTitle()
+        System.out.println("-- After swap (no effect) --");
+        System.out.println("jungleDVD title: " + jungleDVD.getTitle());
+        System.out.println("cinderellaDVD title: " + cinderellaDVD.getTitle());
+
+        // Test changeTitle
         changeTitle(jungleDVD, cinderellaDVD.getTitle());
-        System.out.println("jungle dvd title: " + jungleDVD.getTitle());
+
+        System.out.println("-- After changeTitle --");
+        System.out.println("jungleDVD title: " + jungleDVD.getTitle());
+        System.out.println("cinderellaDVD title: " + cinderellaDVD.getTitle());
     }
 
-    // Phương thức swap (không hoạt động như mong đợi)
-    public static void swap(Object o1, Object o2) {
-        Object tmp = o1;
-        o1 = o2;
-        o2 = tmp;
+    public static void swap(DigitalVideoDisc d1, DigitalVideoDisc d2) {
+        DigitalVideoDisc tmp = d1;
+        d1 = d2;
+        d2 = tmp;
     }
-
-    // Phương thức changeTitle
     public static void changeTitle(DigitalVideoDisc dvd, String title) {
         String oldTitle = dvd.getTitle();
         dvd.setTitle(title);
-        dvd = new DigitalVideoDisc(oldTitle); // Không ảnh hưởng đến đối tượng gốc
+
+        dvd = new DigitalVideoDisc(oldTitle);
     }
 }
