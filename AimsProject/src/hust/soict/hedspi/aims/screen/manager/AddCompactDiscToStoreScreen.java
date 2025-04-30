037a1439398b63b9869fed012f044b03e38b4f5a
IncuksukEi <incuksuk123@gmail.com>
2025-04-30 20:45:55 +0700

    add sreen/manager

--- AimsProject/src/hust/soict/hedspi/aims/screen/manager/AddCompactDiscToStoreScreen.java
+++ AimsProject/src/hust/soict/hedspi/aims/screen/manager/AddCompactDiscToStoreScreen.java
@@ -1,4 +1,7 @@
 package hust.soict.hedspi.aims.screen.manager;
 
-public class AddCompactDiscToStoreScreen {
+public class AddCompactDiscToStoreScreen extends AddItemToStoreScreen {
+    public AddCompactDiscToStoreScreen() {
+        super();
+    }
 }
--- AimsProject/src/hust/soict/hedspi/aims/screen/manager/StoreManagerScreen.java
+++ AimsProject/src/hust/soict/hedspi/aims/screen/manager/StoreManagerScreen.java
@@ -1,4 +1,104 @@
 package hust.soict.hedspi.aims.screen.manager;
 
-public class StoreManagerScreen {
+import javax.swing.*;
+import java.awt.*;
+
+import hust.soict.hedspi.aims.media.Book;
+import hust.soict.hedspi.aims.media.CompactDisc;
+import hust.soict.hedspi.aims.media.DigitalVideoDisc;
+import hust.soict.hedspi.aims.store.Store;
+import hust.soict.hedspi.aims.media.Media;
+import hust.soict.hedspi.aims.*;
+
+public class StoreManagerScreen extends JFrame {
+    private Store store;
+
+    public StoreManagerScreen(Store store) {
+        this.store = store;
+
+        Container cp = getContentPane();
+        cp.setLayout(new BorderLayout());
+
+        cp.add(createNorth(), BorderLayout.NORTH);
+        cp.add(createCenter(), BorderLayout.CENTER);
+
+        setTitle("Store Manager");
+        setSize(1024, 768);
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setVisible(true);
+    }
+    JPanel createNorth() {
+        JPanel north = new JPanel();
+        north.setLayout(new BoxLayout(north, BoxLayout.Y_AXIS));
+
+        JMenuBar menuBar = createMenuBar();
+        JPanel header = createHeader();
+
+        north.add(menuBar);
+        north.add(header);
+
+        return north;
+    }
+
+    JMenuBar createMenuBar() {
+        JMenu menu = new JMenu("Options");
+
+        JMenu smUpdateStore = new JMenu("Update Store");
+        JMenuItem addBook = new JMenuItem("Add Book");
+        JMenuItem addCD = new JMenuItem("Add CD");
+        JMenuItem addDVD = new JMenuItem("Add DVD");
+
+        smUpdateStore.add(addBook);
+        smUpdateStore.add(addCD);
+        smUpdateStore.add(addDVD);
+
+        menu.add(smUpdateStore);
+        menu.add(new JMenuItem("View Store"));
+
+        JMenuBar menuBar = new JMenuBar();
+        menuBar.setLayout(new FlowLayout(FlowLayout.LEFT));
+        menuBar.add(menu);
+
+        return menuBar;
+    }
+
+    JPanel createHeader() {
+        JPanel header = new JPanel();
+        header.setLayout(new BoxLayout(header, BoxLayout.X_AXIS));
+
+        JLabel title = new JLabel("AIMS", JLabel.CENTER);
+        title.setFont(new Font(title.getFont().getName(), Font.PLAIN, 50));
+        title.setForeground(Color.CYAN);
+
+        header.add(Box.createRigidArea(new Dimension(10, 10)));
+        header.add(title);
+        header.add(Box.createRigidArea(new Dimension(10, 10)));
+
+        return header;
+    }
+    JPanel createCenter() {
+        JPanel center = new JPanel();
+        center.setLayout(new GridLayout(3, 3, 2, 2));
+
+        for (Media m : store.getItemsInStore()) {
+            MediaStore cell = new MediaStore(m);
+            center.add(cell);
+        }
+
+        return center;
+    }
+
+    public static void main(String[] args) {
+        Store store = new Store();
+        // Thêm sản phẩm thử
+        store.addMedia(new DigitalVideoDisc("DVD Title", "Category", "Director", 120, 15.5f));
+        // Thêm sách mẫu
+        store.addMedia(new Book(1,"The Lord of the Rings", "Fantasy", 12.99f));
+        store.addMedia(new Book(2,"Effective Java", "Programming", 35.50f));
+        // Thêm DVD mẫu
+        store.addMedia(new DigitalVideoDisc("Inception", "Sci-Fi", "Christopher Nolan", 148, 19.99f));
+        store.addMedia(new DigitalVideoDisc("Interstellar", "Sci-Fi", "Christopher Nolan", 169, 21.50f));
+        new StoreManagerScreen(store);
+    }
 }
